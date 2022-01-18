55daf253b2cc2d3a7da294b4f22dc5cae00313be
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-01-18 12:34:09 +0000

    depart

---
+++ libs/system/Controller.php
@@ -0,0 +1,10 @@
+<?php
+namespace libs\system;
+// use libs\system\View;
+class Controller {
+    protected $view ;
+    public function __construct()
+    {
+        $this->view =new \libs\system\View();
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php
+echo "projet sen-forage";
